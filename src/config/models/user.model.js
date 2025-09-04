58b74242f1a5d128c03c4ec42ed9b9c1f6bc0b5e
panxo87sl <savage87.sl@gmail.com>
2025-09-04 18:09:37 -0400

    Esquema inicial de trabajo

---
+++ src/config/models/user.model.js
@@ -0,0 +1,38 @@
+// src/models/User.model.js
+
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true, //? El correo debe ser único
+    required: true,
+  },
+  age: {
+    type: Number,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cart: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Carts", //? Referencia a la colección "Carts" para el futuro
+  },
+  role: {
+    type: String,
+    default: "user", //? Valor por defecto si no se indica
+  },
+});
+
+const UserModel = mongoose.model("User", userSchema);
+
+export default UserModel;
